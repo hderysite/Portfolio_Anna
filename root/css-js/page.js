98edd5097e9aec9a9f3c0fa6d50e3b22a6a803cd
Anna Carolina <annapachevo890@gmail.com>
2025-09-03 22:26:40 -0300

    feat: adicionando javascript

---
+++ root/css-js/page.js
@@ -0,0 +1,6 @@
+ScrollReveal({ reset: true, distance: '60px', duration: 1200, delay: 200 });
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .service-box, .project-card', { origin: 'bottom' });
+ScrollReveal().reveal('.about-img', { origin: 'left' });
+ScrollReveal().reveal('.about-content, .contact form', { origin: 'right' });
